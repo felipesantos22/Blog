e11ff0e8e4df2054c8c769b0612022ad5566373a
felipesantos22 <felipevs2018@gmail.com>
2024-06-12 19:00:08 -0300

    add new page pages

---
+++ src/app/pages/new-blog/new-blog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-blog',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-blog.component.html',
+  styleUrl: './new-blog.component.css'
+})
+export class NewBlogComponent {
+
+}
